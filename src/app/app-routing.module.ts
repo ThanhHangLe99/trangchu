999b1e4501f2196e11af43304509e46742e39891
ThanhHangLe99 <70497427+ThanhHangLe99@users.noreply.github.com>
2020-10-12 13:55:01 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './customer/cart/cart.component';
+import { CheckoutComponent } from './customer/checkout/checkout.component';
+import { LoginComponent } from './customer/login/login.component';
+import { MainComponent } from './main/main.component';
+import { ChitietComponent } from './product/chitiet/chitiet.component';
+import { ListComponent } from './product/list/list.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MainComponent },
+{ path: 'main', component: MainComponent},
+{ path: 'login', component: LoginComponent },
+{ path: 'chitiet', component: ChitietComponent },
+{ path: 'list', component: ListComponent },
+{ path: 'cart', component: CartComponent },
+{ path: 'checkout', component : CheckoutComponent },
+{ path: '**', component: NotFoundComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { ChitietComponent } from './product/chitiet/chitiet.component';
+import { ListComponent } from './product/list/list.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    NotFoundComponent,
+    ChitietComponent,
+    ListComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
