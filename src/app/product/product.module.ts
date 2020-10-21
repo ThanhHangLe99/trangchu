a70942d9d568662986649db2837dd9c7ae6974e4
ThanhHangLe99 <70497427+ThanhHangLe99@users.noreply.github.com>
2020-10-21 09:29:54 +0700

    update

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,10 @@
+import { ListBrandComponent } from './list-brand/list-brand.component';
 import { DetailComponent } from './detail/detail.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
-//import { ListBrandComponent } from './list-brand/list-brand.component';
-// import { ListBrandComponent } from './list-brand/list-brand.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 const routes: Routes = [
@@ -17,18 +17,19 @@ const routes: Routes = [
     path: 'list/:id',
     component: ListComponent,
   },
-  // {
-  //   path: 'list-brand/:id',
-  //   component: ListBrandComponent,
-  // },
+  {
+    path: 'list-brand/:id',
+    component: ListBrandComponent,
+  },
 ];
 
 
 @NgModule({
-  declarations: [DetailComponent, ListComponent],
+  declarations: [DetailComponent, ListComponent, ListBrandComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    NgbModule
   ]
 })
 export class ProductModule { }
