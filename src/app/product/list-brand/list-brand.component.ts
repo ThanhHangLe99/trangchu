56cdec54e4f7652c63e6cf733d40f4c14dc36f44
ThanhHangLe99 <70497427+ThanhHangLe99@users.noreply.github.com>
2020-11-16 00:09:36 +0700

    update

--- src/app/product/list-brand/list-brand.component.ts
+++ src/app/product/list-brand/list-brand.component.ts
@@ -21,7 +21,7 @@ export class ListBrandComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.list_brand = [];
     this.page = 1;
-    this.pageSize = 5;
+    this.pageSize = 8;
     this._route.params.subscribe(params => {
       this.brand_id = params['id'];
       this._api.post('/api/product/search1', {
